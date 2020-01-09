cbae9afa8b52b641d4acb5786b3a68fba3cdd086
Unaisul Hadi <unaisulhadi@gmail.com>
2020-01-09 16:18:59 +0530

    Sharing

--- app/src/main/java/com/hadi/inspire/Activity/MainActivity.kt
+++ app/src/main/java/com/hadi/inspire/Activity/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.hadi.inspire.Activity
 
 import android.Manifest
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.StrictMode
 import android.provider.Settings
 import android.util.Log
 import android.view.View
@@ -38,6 +43,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.util.*
 
 
@@ -47,10 +54,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var background: View
     lateinit var mRevealAnimation: RevealAnimation
     val colorList = arrayListOf<Int>()
+    lateinit var adapter: QuoteAdapter
+    var rv_pos = 0;
+    private var sharePath = "no"
+    internal var dirPath = ""
+    lateinit var file_loc: File
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val builder = StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure()
 
         this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -61,9 +78,23 @@ class MainActivity : AppCompatActivity() {
         background = findViewById(R.id.bg_)
         val intent =
             this.intent //get the intent to receive the x and y coords, that you passed before
-        val rootLayout = findViewById<RelativeLayout>(R.id.bg_) //there you have to get the root layout of your second activity
+        val rootLayout =
+            findViewById<RelativeLayout>(R.id.bg_) //there you have to get the root layout of your second activity
         mRevealAnimation = RevealAnimation(rootLayout, intent, this)
 
+
+
+        file_loc = File(
+            "" +
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/"
+                    + "Inspire"
+        );
+
+        if (!file_loc.exists()) {
+            file_loc.mkdir()
+        }   
+
+        dirPath = file_loc.absolutePath
 
 
         rv_quote.setHasFixedSize(true)
@@ -85,10 +116,19 @@ class MainActivity : AppCompatActivity() {
 
         ViewPressEffectHelper.attach(btn_share)
         btn_share.setOnClickListener {
-            //Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
             requestReadPermissions()
         }
 
+        rv_quote.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                var offset = rv_quote.computeHorizontalScrollOffset()
+                var cellwidth = rv_quote.getChildAt(0).measuredWidth
+                if (offset % cellwidth == 0) {
+                    rv_pos = offset / cellwidth
+                }
+                //Toast.makeText(this@MainActivity, "$rv_pos", Toast.LENGTH_SHORT).show();
+            }
+        })
     }
 
 
@@ -111,11 +151,15 @@ class MainActivity : AppCompatActivity() {
                     val list = data?.results as ArrayList
                     list.shuffle()
 
-                    val quoteAdapter = QuoteAdapter(this@MainActivity, list, this)
-                    rv_quote.adapter = quoteAdapter
+                    adapter = QuoteAdapter(this@MainActivity, list, this)
+                    rv_quote.adapter = adapter
 
                 } else {
-                    Toast.makeText(this@MainActivity,response.errorBody().toString(),Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        response.errorBody().toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
@@ -139,17 +183,28 @@ class MainActivity : AppCompatActivity() {
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport) { // check if all permissions are granted
                     if (report.areAllPermissionsGranted()) {
-                        Toast.makeText(
-                            applicationContext,
-                            "All permissions are granted!",
-                            Toast.LENGTH_SHORT
-                        ).show()
 
-
+//                        takeSS(bg_)
+                        //var file_ =
+                            //takeScreenshotOfView(rv_quote.findViewHolderForAdapterPosition(rv_pos)!!.itemView)
+                        takeSS(rv_quote.findViewHolderForAdapterPosition(rv_pos)!!.itemView)
+//                        var file  = bitmapToFile(file_)
+//
+//                        Log.d("URI__",file.toString())
+//
+//                        img_item.setImageBitmap(file_)
+//
+//                        val shareIntent: Intent = Intent().apply {
+//                            action = Intent.ACTION_SEND
+//                            putExtra(Intent.EXTRA_STREAM, file)
+//                            type = "image/*"
+//                        }
+//                        startActivity(Intent.createChooser(shareIntent, "Share Quote.."))
                     }
                     // check for permanent denial of any permission
                     if (report.isAnyPermissionPermanentlyDenied) { // show alert dialog navigating to Settings
-                        showSettingsDialog()
+                        //showSettingsDialog()
+                        Toast.makeText(this@MainActivity, "Storage Permission Required.!", Toast.LENGTH_SHORT).show();
                     }
                 }
 
@@ -166,6 +221,125 @@ class MainActivity : AppCompatActivity() {
             .check()
     }
 
+
+
+
+    private fun takeSS(v: View) {
+        val now = Date()
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            val mPath = dirPath + "/" + now + ".jpeg"
+
+            // create bitmap screen capture
+            v.isDrawingCacheEnabled = true
+            val bitmap = Bitmap.createBitmap(v.drawingCache)
+            v.isDrawingCacheEnabled = false
+
+            val imageFile = File(mPath)
+
+            val outputStream = FileOutputStream(imageFile)
+            val quality = 100
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
+
+
+
+            outputStream.flush()
+            outputStream.close()
+
+            //setting screenshot in imageview
+            val filePath = imageFile.path
+
+            val ssbitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+            img_item!!.setImageBitmap(ssbitmap)
+            sharePath = filePath;
+            share(sharePath)
+
+        } catch (e: Throwable) {
+            // Several error may come out with file handling or DOM
+            e.printStackTrace()
+        }
+
+    }
+
+
+    fun takeScreenshotOfView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return bitmap
+    }
+
+    private fun bitmapToFile(bitmap: Bitmap): Uri {
+        // Get the context wrapper
+        val now = Date()
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Initialize a new file instance to save bitmap object
+        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+        file = File(file, "${now}.jpg")
+
+        try {
+            // Compress the bitmap and save in jpg format
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        // Return the saved bitmap uri
+        return Uri.parse(file.absolutePath)
+    }
+
+    private fun getScreenShot(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) bgDrawable.draw(canvas)
+        else canvas.drawColor(Color.WHITE)
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
+
+    private fun share(sharePath: String) {
+
+        val file = File(sharePath)
+        val uri = Uri.fromFile(file)
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_STREAM, uri)
+        }
+//        startActivity(intent)
+        startActivity(Intent.createChooser(intent, "Share Quote.."))
+//        val shareIntent: Intent = Intent().apply {
+//            action = Intent.ACTION_SEND
+//            putExtra(Intent.EXTRA_STREAM, file)
+//            type = "image/*"
+//        }
+//        startActivity(Intent.createChooser(shareIntent, "Share Quote.."))
+
+    }
+
+
+    override fun onBackPressed() {
+        mRevealAnimation.unRevealActivity()
+        //finish()
+    }
+
+    fun closeChoice(view: View) {
+        mRevealAnimation.unRevealActivity()
+    }
 
     private fun showSettingsDialog() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -186,63 +360,5 @@ class MainActivity : AppCompatActivity() {
         val uri = Uri.fromParts("package", packageName, null)
         intent.data = uri
         startActivityForResult(intent, 101)
-    }
-
-    private fun takeSS(v: View?) {
-        val now = Date()
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
-
-        try {
-            // image naming and path  to include sd card  appending name you choose for file
-            val mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpeg"
-
-            // create bitmap screen capture
-            v!!.isDrawingCacheEnabled = true
-            val bitmap = Bitmap.createBitmap(v.drawingCache)
-            v.isDrawingCacheEnabled = false
-
-            val imageFile = File(mPath)
-
-            val outputStream = FileOutputStream(imageFile)
-            val quality = 100
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-            outputStream.flush()
-            outputStream.close()
-
-            //setting screenshot in imageview
-            val filePath = imageFile.path
-
-//            val ssbitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
-//            ivpl!!.setImageBitmap(ssbitmap)
-            //sharePath = filePath;
-
-            share(filePath)
-
-        } catch (e: Throwable) {
-            // Several error may come out with file handling or DOM
-            e.printStackTrace()
-        }
-
-    }
-
-    private fun share(sharePath: String) {
-
-        val file = File(sharePath)
-        val uri = Uri.fromFile(file)
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "image/*"
-        intent.putExtra(Intent.EXTRA_STREAM, uri)
-        startActivity(intent)
-
-    }
-
-
-    override fun onBackPressed() {
-         mRevealAnimation.unRevealActivity()
-        //finish()
-    }
-
-    fun closeChoice(view: View) {
-        mRevealAnimation.unRevealActivity()
     }
 }
--- app/src/main/java/com/hadi/inspire/Adapter/QuoteAdapter.kt
+++ app/src/main/java/com/hadi/inspire/Adapter/QuoteAdapter.kt
@@ -20,6 +20,7 @@ class QuoteAdapter(
     val listener: OnItemClickListener
 ) : RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>() {
 
+    var i = 0;
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
 
@@ -31,11 +32,22 @@ class QuoteAdapter(
     override fun getItemCount() = list.size
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
+
+
+
         holder.quote.text = list[position].quoteText
         holder.quote_by.text = "- ${list[position].quoteAuthor}"
 
 
         ColorStore()
+
+//        if(position <= colorList.size){
+//            i++;
+//            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context, colorList.get(i)));
+//        }
+//        if (position == colorList.size){
+//            i=0;
+//        }
 
         if (position == 0) {
             holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context, R.color.md_blue_400))
@@ -47,91 +59,7 @@ class QuoteAdapter(
         }
 
 
-        /*if(position % 40 == 0){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_400))
-        }else if(position % 40 == 1){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_red_300))
-        }else if(position % 40 == 2){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_pink_300))
-        }else if(position % 40 == 3){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_purple_300))
-        }else if(position % 40 == 4){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_deep_purple_300))
-        }else if(position % 40 == 5){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_indigo_300))
-        }else if(position % 40 == 6){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_300))
-        }else if(position % 40 == 7){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_light_blue_300))
-        }else if(position % 40 == 8){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_cyan_300))
-        }else if(position % 40 == 9){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_teal_300))
-        }else if(position % 40 == 10){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_green_300))
-        }else if(position % 40 == 11){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_light_green_300))
-        }else if(position % 40 == 12){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_lime_300))
-        }else if(position % 40 == 13){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_yellow_300))
-        }else if(position % 40 == 14){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_amber_300))
-        }else if(position % 40 == 15){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_orange_300))
-        }else if(position % 40 == 16){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_deep_orange_300))
-        }else if(position % 40 == 17){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_brown_300))
-        }else if(position % 40 == 18){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_grey_300))
-        }else if(position % 40 == 19){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_grey_300))
-        }
 
-
-
-        if(position % 40 == 0){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_400))
-        }else if(position % 40 == 1){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_red_300))
-        }else if(position % 40 == 2){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_pink_300))
-        }else if(position % 40 == 3){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_purple_300))
-        }else if(position % 40 == 4){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_deep_purple_300))
-        }else if(position % 40 == 5){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_indigo_300))
-        }else if(position % 40 == 6){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_300))
-        }else if(position % 40 == 7){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_light_blue_300))
-        }else if(position % 40 == 8){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_cyan_300))
-        }else if(position % 40 == 9){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_teal_300))
-        }else if(position % 40 == 10){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_green_300))
-        }else if(position % 40 == 11){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_light_green_300))
-        }else if(position % 40 == 12){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_lime_300))
-        }else if(position % 40 == 13){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_yellow_300))
-        }else if(position % 40 == 14){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_amber_300))
-        }else if(position % 40 == 15){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_orange_300))
-        }else if(position % 40 == 16){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_deep_orange_300))
-        }else if(position % 40 == 17){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_brown_300))
-        }else if(position % 40 == 18){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_grey_300))
-        }else if(position % 40 == 19){
-            holder.quote_bg.setBackgroundColor(ContextCompat.getColor(context,R.color.md_blue_grey_300))
-        }*/
     }
 
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
